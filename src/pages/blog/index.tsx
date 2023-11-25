6c1c5735421b9969f5fe837c1ac561595e36a748
freddyal <freddyal0x01@gmail.com>
2023-11-25 13:13:38 -0500

    Added linting, blog folder, & team component

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div>Welcome to our blog</div>
+  )
+}
+
+export default BlogPage
---
+++ src/pages/team.tsx
@@ -0,0 +1,8 @@
+import { BlitzPage } from "@blitzjs/next";
+import React from "react";
+
+const TeamPage: BlitzPage = () => {
+  return <div>This is our team</div>;
+};
+
+export default TeamPage;
