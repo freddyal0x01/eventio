82747aeb9ca58850790ddff6ea3b1d3f20b956f1
freddyal <freddyal0x01@gmail.com>
2023-11-25 23:54:18 -0500

    Installed mantine deps

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,5 @@
 import { ErrorBoundary, AppProps } from "@blitzjs/next";
+import { MantineProvider } from "@mantine/core";
 import React, { Suspense } from "react";
 import { withBlitz } from "src/blitz-client";
 import { RootErrorFallback } from "src/core/components/RootErrorFallback";
@@ -7,7 +8,18 @@ import "src/styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary FallbackComponent={RootErrorFallback}>
-      <Suspense fallback="Loading...">{<Component {...pageProps} />}</Suspense>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "dark",
+        }}
+      >
+        <Suspense fallback="Loading...">
+          {<Component {...pageProps} />}
+        </Suspense>
+      </MantineProvider>
     </ErrorBoundary>
   );
 }
