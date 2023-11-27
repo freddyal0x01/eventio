469e9a5da26a4266579f577e1bdbaa16eba64f0e
freddyal <freddyal0x01@gmail.com>
2023-11-26 22:31:15 -0500

    Added logic for creating an authentication form.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,13 @@
 import Layout from "src/core/layouts/Layout";
 import { BlitzPage } from "@blitzjs/next";
 import { UserInfo } from "src/core/components/UserInfo";
+import { AuthenticationForm } from "src/core/components/MainAuthenticationForm";
 
 const Home: BlitzPage = () => {
   return (
     <Layout title="Home">
       <UserInfo />
+      <AuthenticationForm />
     </Layout>
   );
 };
