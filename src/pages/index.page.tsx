cda32cef6eaeda88da4c8f565c510666d1cf5522
freddyal <freddyal0x01@gmail.com>
2023-11-26 22:36:37 -0500

    Set up form authenticatino when a user is logged out

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,12 +2,20 @@ import Layout from "src/core/layouts/Layout";
 import { BlitzPage } from "@blitzjs/next";
 import { UserInfo } from "src/core/components/UserInfo";
 import { AuthenticationForm } from "src/core/components/MainAuthenticationForm";
+import { useCurrentUser } from "src/features/users/hooks/useCurrentUser";
+import { Vertical } from "mantine-layout-components";
 
 const Home: BlitzPage = () => {
+  const currentUser = useCurrentUser();
+
   return (
     <Layout title="Home">
-      <UserInfo />
-      <AuthenticationForm />
+      {currentUser && <UserInfo />}
+      {!currentUser && (
+        <Vertical center fullW fullH>
+          <AuthenticationForm />
+        </Vertical>
+      )}
     </Layout>
   );
 };
