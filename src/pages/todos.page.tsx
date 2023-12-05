2167d2b9af9b2fc7f304bf331e8cdd80018bc837
freddyal <freddyal0x01@gmail.com>
2023-12-05 17:59:30 -0500

    Added logic to perform todo checklist toggle functionality.

--- src/pages/todos.page.tsx
+++ src/pages/todos.page.tsx
@@ -1,27 +1,37 @@
 import { BlitzPage } from "@blitzjs/next";
 import { useMutation, useQuery } from "@blitzjs/rpc";
-import { Button, Input, List, Loader, Text } from "@mantine/core";
-import { notifications } from "@mantine/notifications";
-import { Vertical } from "mantine-layout-components";
+import { Button, Checkbox, Input, List, Loader, Text } from "@mantine/core";
+import { Horizontal, Vertical } from "mantine-layout-components";
 import { Suspense, useState } from "react";
 import Layout from "src/core/layouts/Layout";
 import addTodo from "src/features/todos/mutations/addTodo";
+import toggleTodo from "src/features/todos/mutations/toggleTodo";
 import getTodos from "src/features/todos/queries/getTodos";
 import { useCurrentUser } from "src/features/users/hooks/useCurrentUser";
+
+const Todo = ({ todo }) => {
+  const [$toggleTodo] = useMutation(toggleTodo);
+  return (
+    <Horizontal>
+      <Checkbox
+        checked={todo.done}
+        onClick={async () => {
+          await $toggleTodo({
+            id: todo.id,
+          });
+        }}
+      />
+      <Text>{todo.title}</Text>
+    </Horizontal>
+  );
+};
 
 const Todos = () => {
   const user = useCurrentUser();
   const [todos] = useQuery(getTodos, {});
   const [todoTitle, setTodoTitle] = useState("");
 
-  const [$addTodo] = useMutation(addTodo, {
-    onSuccess: (todo) => {
-      notifications.show({
-        title: "Mutation Successful",
-        message: `Created todo: ${todo.title}`,
-      });
-    },
-  });
+  const [$addTodo] = useMutation(addTodo, {});
 
   return (
     <Vertical>
@@ -42,9 +52,7 @@ const Todos = () => {
       </Button>
       <List>
         {todos.map((todo) => (
-          <List.Item key={todo.title}>
-            <Text>{todo.title}</Text>
-          </List.Item>
+          <Todo todo={todo} key={todo.id} />
         ))}
       </List>
     </Vertical>
