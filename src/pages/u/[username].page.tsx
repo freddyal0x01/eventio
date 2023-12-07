5ec6de17489bd6f38bfd918b0cfb3971a7f29f0c
freddyal <freddyal0x01@gmail.com>
2023-12-07 16:08:12 -0500

    Created the user profile and username components

--- src/pages/u/[username].page.tsx
+++ src/pages/u/[username].page.tsx
@@ -1,16 +1,32 @@
 import { BlitzPage } from "@blitzjs/next";
-import { Text } from "@mantine/core";
+import { useQuery } from "@blitzjs/rpc";
+import { Button, Text } from "@mantine/core";
 import { Vertical } from "mantine-layout-components";
 import Layout from "src/core/layouts/Layout";
+import { useCurrentUser } from "src/features/users/hooks/useCurrentUser";
+import getUserForProfile from "src/features/users/queries/getUserForProfile";
 import { useStringParam } from "src/utils/utils";
 
 export const ProfilePage: BlitzPage = () => {
   const username = useStringParam("username");
+  const [user] = useQuery(
+    getUserForProfile,
+    { username: username || "" },
+    { enabled: !!username },
+  );
+
+  const currentUser = useCurrentUser();
+
+  const isOwner = currentUser?.id === user?.id;
+
+  if (!user) return <Text>User not found 😞 </Text>;
 
   return (
     <Layout>
       <Vertical>
-        <Text>Hello {username}</Text>
+        {isOwner && <Button>Edit Profile</Button>}
+        <Text>Hello {user.name}</Text>
+        <Text>{user.bio}</Text>
       </Vertical>
     </Layout>
   );
