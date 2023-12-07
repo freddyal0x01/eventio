2025b487d2c2c7a9af1122ab5405a7e1fa4041a9
freddyal <freddyal0x01@gmail.com>
2023-12-06 21:57:39 -0500

    Added user logic whenever a user logs out that they are officially logged out.

--- src/features/users/hooks/useCurrentUser.ts
+++ src/features/users/hooks/useCurrentUser.ts
@@ -3,8 +3,13 @@ import getCurrentUser from "src/features/users/queries/getCurrentUser";
 
 export const useCurrentUser = () => {
   const [user] = useQuery(getCurrentUser, null);
-  return {
-    ...user,
-    isAdmin: user?.role === "ADMIN",
-  };
+
+  if (user) {
+    return {
+      ...user,
+      isAdmin: user?.role === "ADMIN",
+    };
+  }
+
+  return null;
 };
