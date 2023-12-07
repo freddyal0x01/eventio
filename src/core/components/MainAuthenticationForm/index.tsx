d2af627a9f24d259584e3096543fb74a57ecdb28
freddyal <freddyal0x01@gmail.com>
2023-12-06 23:02:33 -0500

    Added a Vertical layout and changed component from AuthenticationForm to MainAuthenticationForm

--- src/core/components/MainAuthenticationForm/index.tsx
+++ src/core/components/MainAuthenticationForm/index.tsx
@@ -14,17 +14,16 @@ import {
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { upperFirst, useToggle } from "@mantine/hooks";
-import { AuthenticationError } from "blitz";
+import { Vertical } from "mantine-layout-components";
 import login from "src/features/auth/mutations/login";
 import signup from "src/features/auth/mutations/signup";
-import { FORM_ERROR } from "../Form";
 import { GoogleButton, TwitterButton } from "./SocialButtons";
 
-export function AuthenticationForm(props: PaperProps) {
+export function MainAuthenticationForm(props: PaperProps) {
   const [type, toggle] = useToggle(["login", "register"]);
 
-  const [loginMutation] = useMutation(login);
-  const [signupMutation] = useMutation(signup);
+  const [$login] = useMutation(login);
+  const [$signup] = useMutation(signup);
 
   const form = useForm({
     initialValues: {
@@ -43,112 +42,89 @@ export function AuthenticationForm(props: PaperProps) {
     },
   });
 
-  const onLogin = async (values) => {
-    try {
-      const user = await loginMutation(values);
-    } catch (error: any) {
-      if (error instanceof AuthenticationError) {
-        return { [FORM_ERROR]: "Sorry, those credentials are invalid" };
-      } else {
-        return {
-          [FORM_ERROR]:
-            "Sorry, we had an unexpected error. Please try again. - " +
-            error.toString(),
-        };
-      }
-    }
-  };
-
-  let onSignup = async (values) => {
-    try {
-      await signupMutation(values);
-    } catch (error: any) {
-      if (error.code === "P2002" && error.meta?.target?.includes("email")) {
-        // This error comes from Prisma
-        return { email: "This email is already being used" };
-      } else {
-        return { [FORM_ERROR]: error.toString() };
-      }
-    }
-  };
-
   const onSubmit = (values) => {
     if (type === "login") {
-      onLogin(values);
+      $login(values);
     } else if (type === "register") {
-      onSignup(values);
+      $signup(values);
     }
   };
 
   return (
-    <Paper radius="md" p="xl" withBorder {...props}>
-      <Text size="lg" fw={500}>
-        Welcome to Eventio, {type} with
-      </Text>
+    <Vertical center fullW fullH>
+      <Paper radius="md" p="xl" withBorder {...props}>
+        <Text size="lg" fw={500}>
+          Welcome to Eventio, {type} with
+        </Text>
 
-      <Group grow mb="md" mt="md">
-        <GoogleButton radius="xl">Google</GoogleButton>
-        <TwitterButton radius="xl">Twitter</TwitterButton>
-      </Group>
+        <Group grow mb="md" mt="md">
+          <GoogleButton radius="xl">Google</GoogleButton>
+          <TwitterButton radius="xl">Twitter</TwitterButton>
+        </Group>
 
-      <Divider label="Or continue with email" labelPosition="center" my="lg" />
+        <Divider
+          label="Or continue with email"
+          labelPosition="center"
+          my="lg"
+        />
 
-      <form onSubmit={form.onSubmit(onSubmit)}>
-        <Stack>
-          {type === "register" && (
+        <form onSubmit={form.onSubmit(onSubmit)}>
+          <Stack>
+            {type === "register" && (
+              <TextInput
+                required
+                label="Name"
+                placeholder="Your name"
+                {...form.getInputProps("name")}
+                radius="md"
+              />
+            )}
+
             <TextInput
               required
-              label="Name"
-              placeholder="Your name"
-              {...form.getInputProps("name")}
+              label="Email"
+              placeholder="hello@mantine.dev"
+              {...form.getInputProps("email")}
               radius="md"
             />
-          )}
 
-          <TextInput
-            required
-            label="Email"
-            placeholder="hello@mantine.dev"
-            {...form.getInputProps("email")}
-            radius="md"
-          />
-
-          <PasswordInput
-            required
-            label="Password"
-            placeholder="Your password"
-            {...form.getInputProps("password")}
-            radius="md"
-          />
-
-          {type === "register" && (
-            <Checkbox
-              label="I accept terms and conditions"
-              checked={form.values.terms}
-              onChange={(event) =>
-                form.setFieldValue("terms", event.currentTarget.checked)
-              }
+            <PasswordInput
+              required
+              label="Password"
+              placeholder="Your password"
+              {...form.getInputProps("password")}
+              radius="md"
             />
-          )}
-        </Stack>
 
-        <Group position="apart" mt="xl">
-          <Anchor
-            component="button"
-            type="button"
-            c="dimmed"
-            onClick={() => toggle()}
-            size="xs"
-          >
-            {type === "register"
-              ? "Already have an account? Login"
-              : "Don't have an account? Register"}
-          </Anchor>
-          <Button type="submit" radius="xl">
-            {upperFirst(type)}
-          </Button>
-        </Group>
-      </form>
-    </Paper>
+            {type === "register" && (
+              <Checkbox
+                label="I accept terms and conditions"
+                checked={form.values.terms}
+                onChange={(event) =>
+                  form.setFieldValue("terms", event.currentTarget.checked)
+                }
+              />
+            )}
+          </Stack>
+
+          <Group position="apart" mt="xl">
+            <Anchor
+              component="button"
+              type="button"
+              c="dimmed"
+              onClick={() => toggle()}
+              size="xs"
+            >
+              {type === "register"
+                ? "Already have an account? Login"
+                : "Don't have an account? Register"}
+            </Anchor>
+            <Button type="submit" radius="xl">
+              {upperFirst(type)}
+            </Button>
+          </Group>
+        </form>
+      </Paper>
+    </Vertical>
   );
 }
