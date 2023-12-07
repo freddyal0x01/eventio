44c0394291eb4223b877b5cc4befbe2f318424aa
freddyal <freddyal0x01@gmail.com>
2023-12-06 23:00:13 -0500

    Start up Main Authentication Form if you're not a user

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,37 +1,12 @@
 import { BlitzPage } from "@blitzjs/next";
-import { useMutation } from "@blitzjs/rpc";
-import { Button } from "@mantine/core";
-import { Vertical } from "mantine-layout-components";
-import { AuthenticationForm } from "src/core/components/MainAuthenticationForm";
+import { MainAuthenticationForm } from "src/core/components/MainAuthenticationForm";
 import Layout from "src/core/layouts/Layout";
-import adminOnlyMutation from "src/features/auth/mutations/adminOnlyMutation";
 import { useCurrentUser } from "src/features/users/hooks/useCurrentUser";
 
 const Home: BlitzPage = () => {
   const user = useCurrentUser();
 
-  const [$adminOnlyMutation] = useMutation(adminOnlyMutation);
-
-  return (
-    <Layout title="Home">
-      {user && (
-        <Vertical>
-          <Button
-            onClick={() => {
-              $adminOnlyMutation({});
-            }}
-          >
-            Admin only button
-          </Button>
-        </Vertical>
-      )}
-      {!user && (
-        <Vertical center fullW fullH>
-          <AuthenticationForm />
-        </Vertical>
-      )}
-    </Layout>
-  );
+  return <Layout title="Home">{!user && <MainAuthenticationForm />}</Layout>;
 };
 
 export default Home;
