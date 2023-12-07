3e7133f13da7e49ca4060931dec8ff67d3d2c5f1
freddyal <freddyal0x01@gmail.com>
2023-12-06 21:55:00 -0500

    Added admin only button for admin users

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,15 +1,31 @@
 import { BlitzPage } from "@blitzjs/next";
+import { useMutation } from "@blitzjs/rpc";
+import { Button } from "@mantine/core";
 import { Vertical } from "mantine-layout-components";
 import { AuthenticationForm } from "src/core/components/MainAuthenticationForm";
 import Layout from "src/core/layouts/Layout";
+import adminOnlyMutation from "src/features/auth/mutations/adminOnlyMutation";
 import { useCurrentUser } from "src/features/users/hooks/useCurrentUser";
 
 const Home: BlitzPage = () => {
-  const currentUser = useCurrentUser();
+  const user = useCurrentUser();
+
+  const [$adminOnlyMutation] = useMutation(adminOnlyMutation);
 
   return (
     <Layout title="Home">
-      {!currentUser && (
+      {user && (
+        <Vertical>
+          <Button
+            onClick={() => {
+              $adminOnlyMutation({});
+            }}
+          >
+            Admin only button
+          </Button>
+        </Vertical>
+      )}
+      {!user && (
         <Vertical center fullW fullH>
           <AuthenticationForm />
         </Vertical>
