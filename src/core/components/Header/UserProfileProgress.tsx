61a137c91aab3054cae56986bac3dc0d83ec95e5
freddyal <freddyal0x01@gmail.com>
2023-12-09 13:33:25 -0500

    Show a user their profile progress

--- src/core/components/Header/UserProfileProgress.tsx
+++ src/core/components/Header/UserProfileProgress.tsx
@@ -1,5 +1,6 @@
 import { Routes } from "@blitzjs/next";
-import { RingProgress, Tooltip } from "@mantine/core";
+import { List, RingProgress, Text, Tooltip } from "@mantine/core";
+import { Vertical } from "mantine-layout-components";
 import Link from "next/link";
 import { useCurrentUser } from "src/features/users/hooks/useCurrentUser";
 
@@ -33,6 +34,8 @@ const UserProfileProgress = () => {
 
   const existingKeys = keys.filter(({ key }) => user[key]);
 
+  const missingKeys = keys.filter(({ key }) => !user[key]);
+
   const completionPercentage = Math.round(
     (existingKeys.length / keys.length) * 100,
   );
@@ -43,13 +46,25 @@ const UserProfileProgress = () => {
     <Link href={Routes.EditProfilePage()}>
       <Tooltip
         color="dark"
-        label={`Profile progress (${completionPercentage}%)`}
+        label={
+          <Vertical spacing={"xs"}>
+            <Text fw={"bold"}>Profile Progress ({completionPercentage}%)</Text>
+            <Vertical spacing={0}>
+              <Text>Missing:</Text>
+              <List>
+                {missingKeys.map(({ label }) => (
+                  <List.Item key={label}>{label}</List.Item>
+                ))}
+              </List>
+            </Vertical>
+          </Vertical>
+        }
       >
         <RingProgress
           size={25}
           thickness={4}
           roundCaps
-          sections={[{ value: completionPercentage, color: "cyan" }]}
+          sections={[{ value: completionPercentage, color: "red" }]}
         />
       </Tooltip>
     </Link>
