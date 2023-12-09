a5bc98c254e96e039d846fbf383d75670203205d
freddyal <freddyal0x01@gmail.com>
2023-12-08 21:35:59 -0500

    Finished the backend logic for verifying emails.

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const isDev = process.env.NODE_ENV === "development";
-const PROD_URL = "https://eventio.com";
+const PROD_URL = "https://eventio-prod.up.railway.app";
 const DEV_URL = "http://localhost:3000";
 export const URL_ORIGIN = isDev ? DEV_URL : PROD_URL;
