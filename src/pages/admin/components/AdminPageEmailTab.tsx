57ef3a3c2df106f9e5ee1bc0041fd5aa483f55dd
freddyal <freddyal0x01@gmail.com>
2023-12-16 11:59:41 -0500

    Added functionality to change values in an email template

--- src/pages/admin/components/AdminPageEmailTab.tsx
+++ src/pages/admin/components/AdminPageEmailTab.tsx
@@ -1,11 +1,16 @@
 import { BlitzPage } from "@blitzjs/next";
 import { useMutation } from "@blitzjs/rpc";
-import { Button, Select } from "@mantine/core";
+import { ActionIcon, Button, Input, Select } from "@mantine/core";
+import { IconTrash } from "@tabler/icons-react";
 import { Horizontal, Vertical } from "mantine-layout-components";
 import { useState } from "react";
+import { UseArray, useArray } from "react-hanger";
 import sendBulkEmail from "src/features/email/mutations/sendBulkEmail";
 import { emailTemplates } from "src/features/email/templates";
 import { EmailList, EmailTemplate } from "src/features/email/types";
+import { updateArrayMemberById } from "src/utils/react-hanger-utils";
+import { convertArrayToObject } from "src/utils/utils";
+import { ReactFC } from "types";
 
 const options = [
   { value: EmailList.Marketing, label: "Marketing" },
@@ -13,14 +18,99 @@ const options = [
   { value: EmailList.All, label: "All" },
 ];
 
+type VariableType = {
+  id: string;
+  key: string;
+  value: string;
+};
+
+type Variables = UseArray<any>;
+
+const Variable: ReactFC<{ variable: VariableType; variables: Variables }> = ({
+  variable,
+  variables,
+}) => {
+  return (
+    <Horizontal>
+      <ActionIcon
+        color="red"
+        onClick={() => {
+          variables.removeById(variable.id);
+        }}
+      >
+        <IconTrash size={13} />
+      </ActionIcon>
+      <Input
+        onChange={(e) => {
+          updateArrayMemberById({
+            array: variables,
+            id: variable.id,
+            update: {
+              key: e.target.value,
+            },
+          });
+        }}
+        placeholder="Key"
+        value={variable.key}
+      />
+      <Input
+        onChange={(e) => {
+          updateArrayMemberById({
+            array: variables,
+            id: variable.id,
+            update: {
+              value: e.target.value,
+            },
+          });
+        }}
+        placeholder="Value"
+        value={variable.value}
+      />
+    </Horizontal>
+  );
+};
+
+const VariablesManager: ReactFC<{
+  variables: Variables;
+}> = ({ variables }) => {
+  return (
+    <Vertical>
+      <Button
+        onClick={() =>
+          variables.push({
+            id: Math.random().toString(),
+            key: "",
+            value: "",
+          })
+        }
+      >
+        Add a variable
+      </Button>
+
+      <Vertical>
+        {variables.value.map((v) => (
+          <Variable variables={variables} variable={v} key={v.id} />
+        ))}
+      </Vertical>
+    </Vertical>
+  );
+};
+
 export const AdminPageEmailTab: BlitzPage = () => {
   const [list, setList] = useState<EmailList>(EmailList.Marketing);
   const [template, setTemplate] = useState<EmailTemplate>(
     emailTemplates[0]!.value,
   );
+
+  const variables = useArray([]);
+
   const [$sendBulkEmail] = useMutation(sendBulkEmail);
 
   const foundTemplate = emailTemplates.find((t) => t.value === template);
+
+  const componentProps = convertArrayToObject(variables.value);
+
+  console.log("Component Props", componentProps);
 
   return (
     <Horizontal align="flex-start" mih={"100vh"} fullH>
@@ -44,6 +134,7 @@ export const AdminPageEmailTab: BlitzPage = () => {
             setTemplate(value as EmailTemplate);
           }}
         />
+        <VariablesManager variables={variables} />
         <Button
           onClick={() => {
             $sendBulkEmail({ list, template });
@@ -52,8 +143,8 @@ export const AdminPageEmailTab: BlitzPage = () => {
           Send bulk email
         </Button>
       </Vertical>
-      {/* @ts-ignore */}
-      {foundTemplate && <foundTemplate.component />}
+      {/*@ts-ignore*/}
+      {foundTemplate && <foundTemplate.component props={componentProps} />}
     </Horizontal>
   );
 };
