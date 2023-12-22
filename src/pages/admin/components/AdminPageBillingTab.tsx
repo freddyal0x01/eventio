0dc440f4c38f199e1796a6d49b6b36d9b64ba052
freddyal <freddyal0x01@gmail.com>
2023-12-22 15:00:15 -0500

    Created admin billing tab and fixed space in schema.prisma

--- src/pages/admin/components/AdminPageBillingTab.tsx
+++ src/pages/admin/components/AdminPageBillingTab.tsx
@@ -1,3 +1,4 @@
+import { BlitzPage } from "@blitzjs/next";
 import { useMutation, useQuery } from "@blitzjs/rpc";
 import { Button, Table, Title } from "@mantine/core";
 import { Vertical } from "mantine-layout-components";
@@ -5,12 +6,15 @@ import getLemonSqueezyProducts from "src/features/lemon-squeezy-products/queries
 import getLemonSqueezyVariants from "src/features/lemon-squeezy-variants/queries/getLemonSqueezyVariants";
 import populateLemonSqueezyInfo from "src/features/payments/mutations/populateLemonSqueezyInfo";
 
-export const AdminPageBillingTab = () => {
+export const AdminPageBillingTab: BlitzPage = () => {
   const [$populate, { isLoading }] = useMutation(populateLemonSqueezyInfo);
   const [products] = useQuery(getLemonSqueezyProducts, {});
   const [variants] = useQuery(getLemonSqueezyVariants, {});
 
-  const rows = products.map((product) => {
+  // console.log(products);
+  // console.log(variants);
+
+  const rows = products.map((product) => (
     <tr key={product.id}>
       <td>{product.id}</td>
       <td>{product.name}</td>
@@ -18,23 +22,34 @@ export const AdminPageBillingTab = () => {
       <td>{product.productId}</td>
       <td>{product._count?.variants}</td>
       <td>{product.attributes ? "Yes" : "No"}</td>
-    </tr>;
-  });
+    </tr>
+  ));
 
-  const variantRows = variants.map((variant) => {
+  const variantRows = variants.map((variant) => (
     <tr key={variant.id}>
       <td>{variant.id}</td>
       <td>{variant.name}</td>
       <td>{variant._count?.subscriptions}</td>
+      <td>{variant.price}</td>
       <td>{variant.variantId}</td>
       <td>{variant.product?.name}</td>
       <td>{variant.attributes ? "Yes" : "No"}</td>
-    </tr>;
-  });
+    </tr>
+  ));
 
   return (
     <Vertical>
-      <Button loading={isLoading} onClick={() => $populate({})}>
+      <Button
+        loading={isLoading}
+        onClick={async () => {
+          try {
+            const result = await $populate();
+            console.log(result);
+          } catch (err) {
+            console.log(err);
+          }
+        }}
+      >
         Populate
       </Button>
 
@@ -46,9 +61,8 @@ export const AdminPageBillingTab = () => {
               <th>ID</th>
               <th>Name</th>
               <th>Subscriptions</th>
-              <th>Price</th>
               <th>Product ID</th>
-              <th>Product</th>
+              <th>Variants</th>
               <th>Attributes</th>
             </tr>
           </thead>
