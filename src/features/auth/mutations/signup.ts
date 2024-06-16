9869a6af428aa25a4f8c24a173fa4fa071c87723
freddyal <freddyal0x01@gmail.com>
2024-06-16 13:15:02 -0400

    Added email invite functionality

--- src/features/auth/mutations/signup.ts
+++ src/features/auth/mutations/signup.ts
@@ -4,6 +4,7 @@ import db from "db";
 import { EmailTemplateWelcome } from "email/react-email/emails/welcome";
 import { sendEmail } from "email/sendEmail";
 import { createElement } from "react";
+import { PrismaError } from "src/utils/blitz-utils";
 import { Role } from "types";
 import { SignupInput } from "../schemas";
 
@@ -12,27 +13,45 @@ export default resolver.pipe(
   async ({ email, name, password }, ctx) => {
     const hashedPassword = await SecurePassword.hash(password.trim());
 
-    const user = await db.user.create({
-      data: {
+    const existingInvite = await db.signupInvite.findFirst({
+      where: {
         email: email.toLowerCase().trim(),
-        name,
-        hashedPassword,
-        role: "USER",
-        onboarded: false,
       },
-      select: { id: true, name: true, email: true, role: true },
     });
 
-    if (user) {
-      await sendEmail({
-        to: user.email,
-        subject: "Welcome to Eventio!",
-        react: createElement(EmailTemplateWelcome, {
-          props: { name: user.name, emailVerifyUrl: "" },
-        }),
+    if (!existingInvite) {
+      throw new Error("No invite found for this email address");
+    }
+
+    if (!existingInvite.accepted) {
+      throw new Error("Invite has not been accepted yet");
+    }
+
+    try {
+      const user = await db.user.create({
+        data: {
+          email: email.toLowerCase().trim(),
+          name,
+          hashedPassword,
+          role: "USER",
+          onboarded: false,
+        },
+        select: { id: true, name: true, email: true, role: true },
       });
-      await ctx.session.$create({ userId: user.id, role: user.role as Role });
-      return user;
+
+      if (user) {
+        await sendEmail({
+          to: user.email,
+          subject: "Welcome to Eventio!",
+          react: createElement(EmailTemplateWelcome, {
+            props: { name: user.name, emailVerifyUrl: "" },
+          }),
+        });
+        await ctx.session.$create({ userId: user.id, role: user.role as Role });
+        return user;
+      }
+    } catch (err) {
+      throw new PrismaError(err.message, err.code, err.meta);
     }
 
     return null;
